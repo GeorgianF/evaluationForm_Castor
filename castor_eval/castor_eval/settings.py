547cf03bafe3dabe3b5e78af251e04e09b522f6f
Andreea Furtuna <91877102+GeorgianF@users.noreply.github.com>
2024-05-20 12:21:48 +0200

    Add secret key in env

--- castor_eval/castor_eval/settings.py
+++ castor_eval/castor_eval/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tqv11y-e_7o0&$@3w00(f69+9uj1f!x3y6yzl#@%z694p%p%eg'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -116,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
---
+++ env.py
@@ -0,0 +1,1 @@
+SECRET_KEY = 'django-insecure-tqv11y-e_7o0&$@3w00(f69+9uj1f!x3y6yzl#@%z694p%p%eg'
