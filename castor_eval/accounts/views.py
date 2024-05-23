84605c80cbaa78c295a3b2e08ccdb9801018e650
Andreea Furtuna <91877102+GeorgianF@users.noreply.github.com>
2024-05-23 22:12:28 +0200

    add logout view

--- castor_eval/accounts/views.py
+++ castor_eval/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.contrib import auth
 
 
 # render the login page
@@ -17,4 +19,11 @@ def login_view(request):
         else:
             messages.error(request, 'Invalid email or password. Please try again.')
             return redirect('login')
+    return render(request, 'index.html')
+
+
+@login_required(login_url='login')
+def logout_view(request):
+    auth.logout(request)
+    messages.success(request, 'You are now logged out.')
     return render(request, 'index.html')
